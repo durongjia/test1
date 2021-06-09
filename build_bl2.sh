d65394a15dff25c63a27ec88671cb1e6f95376f4
Julien Masson <jmasson@baylibre.com>
2021-06-09 11:03:34 +0200

    bl2: use libdram library at compilation
    libdram.a has to be passed as argument when we compile bl2.
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- build_bl2.sh
+++ build_bl2.sh
@@ -2,6 +2,7 @@
 
 SRC=$(dirname $(readlink -e "$0"))
 source "${SRC}/utils.sh"
+source "${SRC}/build_libdram.sh"
 
 function clean_bl2 {
     local MTK_PLAT="$1"
@@ -12,15 +13,20 @@ function build_bl2 {
     local MTK_PLAT=$(config_value "$1" plat)
     local ATF_PROJECT=$(config_value "$1" bl2.project)
     local MTK_CFLAGS=$(config_value "$1" bl2.cflags)
-
+    local MTK_LIBDRAM_BOARD=$(config_value "$1" libdram.board)
+    local LIBDRAM_A="${LIBDRAM}/build-${MTK_LIBDRAM_BOARD}/src/${MTK_PLAT}/libdram.a"
     local clean="$2"
+
     ! [ -d "${OUT}/${MTK_PLAT}" ] && mkdir "${OUT}/${MTK_PLAT}"
+
+    # check if libdram has been compiled
+    ! [ -a "${LIBDRAM_A}" ] && build_libdram "$1"
 
     pushd "${ROOT}/${ATF_PROJECT}"
     [[ "${clean}" == true ]] && clean_bl2 "${MTK_PLAT}"
 
     aarch64_env
-    make E=0 CFLAGS="${MTK_CFLAGS}" PLAT="${MTK_PLAT}" bl2
+    make E=0 CFLAGS="${MTK_CFLAGS}" PLAT="${MTK_PLAT}" LIBDRAM="${LIBDRAM_A}" bl2
 
     pushd "build/${MTK_PLAT}/release"
     cp bl2.bin bl2.img.tmp
