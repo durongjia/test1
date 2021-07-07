5ac9a9e0a6f0a07a37a0d3a563696b50fe486f33
Guillaume La Roque <glaroque@baylibre.com>
2021-07-07 12:02:51 +0200

    build_optee: add board field support
    add support of different board name between optee and other component
    if board field is added in optee section with use it instead we use plat
    value.
    
    Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

--- build_optee.sh
+++ build_optee.sh
@@ -12,6 +12,7 @@ function clean_optee {
 function build_optee {
     local MTK_PLAT=$(config_value "$1" plat)
     local OPTEE_FLAGS=$(config_value "$1" optee.flags)
+    local OPTEE_BOARD=$(config_value "$1" optee.board)
     local clean="$2"
 
     ! [ -d "${OUT}/${MTK_PLAT}" ] && mkdir -p "${OUT}/${MTK_PLAT}"
@@ -20,7 +21,11 @@ function build_optee {
     [[ "${clean}" == true ]] && clean_optee "${MTK_PLAT}"
 
     aarch64_env
-    make -j$(nproc) PLATFORM="mediatek-${MTK_PLAT}" $OPTEE_FLAGS all
+    if [ -n "${OPTEE_BOARD}" ]; then
+        make -j$(nproc) PLATFORM="mediatek-${OPTEE_BOARD}" $OPTEE_FLAGS all
+    else
+        make -j$(nproc) PLATFORM="mediatek-${MTK_PLAT}" $OPTEE_FLAGS all
+    fi
     cp out/arm-plat-mediatek/core/tee.bin "${OUT}/${MTK_PLAT}/"
 
     clear_vars
