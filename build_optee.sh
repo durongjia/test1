5d441c9881e18a5e1cbd84ca42411844c0b3c8ae
Julien Masson <jmasson@baylibre.com>
2022-02-01 18:53:57 +0100

    optee: add support to build/include early TA
    The user can now specify a list of Trusted Applications to build and
    include to the tee binary.
    The list contains paths which are relative to the top directory.
    
    Yaml config example:
    optee:
      early_ta_list: optee-avb/023f8f1a-292a-432b-8fc4-de8471358067.elf optee-otp/ta/3712bdda-569f-4940-b749-fb3b06a5fd86.elf
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- build_optee.sh
+++ build_optee.sh
@@ -15,6 +15,15 @@ function clean_optee {
     fi
 }
 
+function build_ta {
+    local optee_flags="$1"
+    local ta_dir=$(dirname "$2")
+
+    pushd "${ta_dir}"
+    make -j"$(nproc)" ${optee_flags}
+    popd
+}
+
 function build_optee {
     local mtk_plat=$(config_value "$1" plat)
     local optee_flags=$(config_value "$1" optee.flags)
@@ -22,6 +31,8 @@ function build_optee {
     local clean="${2:-false}"
     local mode="${3:-release}"
     local out_dir=$(out_dir "$1" "${mode}")
+    local optee_out_dir="${OPTEE}/out/arm-plat-mediatek"
+    local early_ta_paths=($(config_value "$1" optee.early_ta_paths))
 
     display_current_build "$1" "optee" "${mode}"
 
@@ -45,6 +56,18 @@ function build_optee {
     [[ "${clean}" == true ]] && clean_optee "${mtk_plat}"
 
     aarch64_env
+
+    # build early TA
+    if [ "${#early_ta_paths[@]}" -gt 0 ]; then
+        make -j"$(nproc)" ${optee_flags} ta_dev_kit
+        export TA_DEV_KIT_DIR="${optee_out_dir}/export-ta_arm64"
+
+        early_ta_paths=("${early_ta_paths[@]/#/${ROOT}/}")
+        for early_ta in "${early_ta_paths[@]}"; do
+            build_ta "${optee_flags}" "${early_ta}"
+            optee_flags+=" EARLY_TA_PATHS+=${early_ta}"
+        done
+    fi
 
     # build tee binary
     make -j"$(nproc)" ${optee_flags} all
