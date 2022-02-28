a201c6ee7f6fcb04619043b4605d9e5ab4e6441d
Julien Masson <jmasson@baylibre.com>
2022-02-28 12:48:10 +0100

    secure: avb: add support for private key
    The private key for Android Verified Boot (avb) can now be retrieved
    with get_avb_key function.
    The user has 2 choices concerning the avb private key:
    1. Add absolute path in yaml config:
    secure:
      avb_key: /home/julien/Documents/avb.pem
    
    2. Copy avb private key in KEYS directory: ${BUILD}/.keys/avb.pem
    
    Signed-off-by: Julien Masson <jmasson@baylibre.com>

--- secure.sh
+++ secure.sh
@@ -9,6 +9,9 @@ source "${SRC}/utils.sh"
 
 KEYS="${BUILD}/.keys"
 ROT_KEY="rot_key.pem"
+
+# Android Verified Boot (AVB)
+AVB_KEY="avb.pem"
 AVB_PUB_KEY="avb_pub.pem"
 
 function generate_rot_key {
@@ -35,6 +38,23 @@ function get_rot_key {
     fi
 }
 
+function get_avb_key {
+    local avb_key_config=$(config_value "$1" secure.avb_key)
+    local -n avb_key_ref="$2"
+
+    if [ -n "${avb_key_config}" ]; then
+        if [ -a "${avb_key_config}" ]; then
+            avb_key_ref="${avb_key_config}"
+        else
+            error_exit "AVB key not found: ${avb_key_config}"
+        fi
+    else
+        if [ -a "${KEYS}/${AVB_KEY}" ]; then
+            avb_key_ref="${KEYS}/${AVB_KEY}"
+        fi
+    fi
+}
+
 function get_avb_pub_key {
     local avb_pub_key_config=$(config_value "$1" secure.avb_pub_key)
     local -n avb_pub_key_ref="$2"
@@ -53,15 +73,15 @@ function get_avb_pub_key {
 }
 
 function generate_avb_keys {
+    local avb_key="${KEYS}/${AVB_KEY}"
     local avb_pub_key="${KEYS}/${AVB_PUB_KEY}"
-    local avb_priv_key="${KEYS}/avb_priv.pem"
 
     ! [ -d "${KEYS}" ] && mkdir -p "${KEYS}"
 
-    openssl genrsa -out "${avb_priv_key}" 4096
-    avbtool extract_public_key --key "${avb_priv_key}" --output "${avb_pub_key}"
+    openssl genrsa -out "${avb_key}" 4096
+    avbtool extract_public_key --key "${avb_key}" --output "${avb_pub_key}"
 
-    printf "AVB keys generated here:\n%s\n%s\n" "${avb_priv_key}" "${avb_pub_key}"
+    printf "AVB keys generated here:\n%s\n%s\n" "${avb_key}" "${avb_pub_key}"
 }
 
 # main
