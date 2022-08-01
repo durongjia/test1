BayLibre/android_bootloader_build

release_android: add --no-build flag
As of today, release_android.sh does a little more than "releasing"
binaries. It will also rebuild everything from source. This makes it
less useable in automated environments (such as CI) where we want fine
control over the build/deploy steps. For example:
* task 1 builds everything running build_all.sh
* task 2 releases everything (commiting binaries to repos)

Add a --no-build option to release_android.sh so that we can avoid
rebuilding while releasing.

Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

release_android.sh
@@ -141,6 +141,7 @@ Options:
   --commit   (OPTIONAL) commit binaries in AOSP
   --config   (OPTIONAL) release ONLY for this board config file
   --help     (OPTIONAL) display usage
+  --no-build (OPTIONAL) don't rebuild the images
   --silent   (OPTIONAL) silent build commands
 
 The changes specified in the commit msg can be read from:
@@ -152,10 +153,11 @@ function main {
     local aosp=""
     local commit=false
     local config=""
+    local build=true
     local silent=false
     local mode_list=(debug release)
 
-    local opts_args="aosp:,commit,config:,help,silent"
+    local opts_args="aosp:,commit,config:,help,no-build,silent"
     local opts=$(getopt -o '' -l "${opts_args}" -- "$@")
     eval set -- "${opts}"
 
@@ -169,6 +171,7 @@ function main {
                 shift 2 ;;
             --help) usage; exit 0 ;;
             --silent) silent=true; shift ;;
+            --no-build) build=false; shift ;;
             --) shift; break ;;
         esac
     done
@@ -199,11 +202,13 @@ function main {
             ti_binaries_path=$(config_value "${ti_config}" android.binaries_path)
             out_dir=$(out_dir "${ti_config}" "${mode}")
 
-            if [[ "${silent}" == true ]]; then
-                display_current_build "${ti_config}" "all" "${mode}"
-                build_all "${ti_config}" "true" "${mode}" &> /dev/null
-            else
-                build_all "${ti_config}" "true" "${mode}"
+            if [[ "${build}" == true ]]; then
+                if [[ "${silent}" == true ]]; then
+                    display_current_build "${ti_config}" "all" "${mode}"
+                    build_all "${ti_config}" "true" "${mode}" &> /dev/null
+                else
+                    build_all "${ti_config}" "true" "${mode}"
+                fi
             fi
 
             if [ -d "${aosp}/${ti_binaries_path}" ]; then

durongjia/test1

release_android.sh
@@ -127,6 +127,7 @@ Options:
   --config   (OPTIONAL) release ONLY for this board config file
   --help     (OPTIONAL) display usage
   --mode     (OPTIONAL) [release|debug|factory] build only one mode
+  --no-build (OPTIONAL) don't rebuild the images
   --silent   (OPTIONAL) silent build commands
   --skip-ta  (OPTIONAL) skip build Trusted Applications
 
@@ -141,11 +142,12 @@ function main {
     local aosp=""
     local commit=false
     local config=""
+    local build=true
     local silent=false
     local skip_ta=false
     local mode_list=(debug release)
 
-    local opts_args="aosp:,commit,config:,help,mode:,silent,skip-ta"
+    local opts_args="aosp:,commit,config:,help,mode:,no-build,silent,skip-ta"
     local opts=$(getopt -o '' -l "${opts_args}" -- "$@")
     eval set -- "${opts}"
 
@@ -159,6 +161,7 @@ function main {
                 shift 2 ;;
             --help) usage; exit 0 ;;
             --mode) mode_list=("$2"); shift 2 ;;
+            --no-build) build=false; shift ;;
             --silent) silent=true; shift ;;
             --skip-ta) skip_ta=true; shift ;;
             --) shift; break ;;
@@ -192,11 +195,13 @@ function main {
         for mode in "${mode_list[@]}"; do
             ! [[ " ${MODES[*]} " =~ " ${mode} " ]] && error_usage_exit "${mode} mode not supported"
 
-            if [[ "${silent}" == true ]]; then
-                display_current_build "${mtk_config}" "all" "${mode}"
-                build_all "${mtk_config}" "true" "${mode}" &> /dev/null
-            else
-                build_all "${mtk_config}" "true" "${mode}"
+            if [[ "${build}" == true ]]; then
+                if [[ "${silent}" == true ]]; then
+                    display_current_build "${mtk_config}" "all" "${mode}"
+                    build_all "${mtk_config}" "true" "${mode}" &> /dev/null
+                else
+                    build_all "${mtk_config}" "true" "${mode}"
+                fi
             fi
 
             if [ -d "${aosp}/${mtk_binaries_path}" ]; then
@@ -206,7 +211,7 @@ function main {
             fi
 
             # Trusted Applications
-            if [[ "${skip_ta}" == false ]]; then
+            if [[ "${build}" == true ]] && [[ "${skip_ta}" == false ]]; then
                 if [[ "${silent}" == true ]]; then
                     build_android_ta "${mtk_config}" "true" "${mode}" &> /dev/null
                 else