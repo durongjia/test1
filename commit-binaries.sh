62df4b8cdf8321a69ff8913949ceae11f511bd06
Mattijs Korpershoek <mkorpershoek@baylibre.com>
2022-08-04 18:33:53 +0200

    release_android: move commit msg logic to commit-binaries
    By moving all commit message generation logic into a dedicated file, we
    can reuse commit-binaries for other projects, such as the android kernel build.
    
    To transition, move everything related from release_android.sh ->
    commit-binaries.sh and include it.
    
    No functional change
    
    Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

--- commit-binaries.sh
+++ commit-binaries.sh
@@ -28,6 +28,62 @@ Examples:
 DELIM__
 }
 
+function display_center_msg {
+    local line_length="$1"
+    local msg="$2"
+    local length=${#msg}
+    local pad=$(((line_length - length) / 2))
+
+    printf "%0.s " $(seq 1 ${pad})
+    printf "${msg}"
+
+    # if line_length is an odd number, add 1 to pad
+    if [ $(((pad * 2) + length)) != "${line_length}" ]; then
+        pad=$((pad + 1))
+    fi
+
+    printf "%0.s " $(seq 1 ${pad})
+}
+
+function display_commit_msg_header {
+    local path="$1"
+
+    printf "%0.s#" {1..90}
+    printf "\n##"
+    display_center_msg 86 "COMMIT MESSAGE IN:"
+    printf "##\n##"
+    display_center_msg 86 "${path}"
+    printf "##\n"
+    printf "%0.s#" {1..90}
+    printf "\n\n"
+}
+
+function commit_msg_body {
+    local remote_name=$1 && shift
+    local projects=("$@")
+
+    local remote_url=""
+    local head=""
+    local branch=""
+
+    for project in "${projects[@]}"; do
+        pushd "${ROOT}/${project}"
+        body+="- Project: ${project}:\n"
+
+        remote_url=$(git remote get-url "${remote_name}")
+        body+="URL: ${remote_url}\n"
+
+        branch=$(repo --color=never info . 2>&1 | perl -ne 'print "$1" if /^Manifest revision: (.*)/')
+        body+="Branch: ${branch}\n"
+
+        head=$(git log --oneline --no-decorate -1)
+        body+="HEAD: ${head}\n\n"
+        popd
+    done
+
+    echo "${body}"
+}
+
 function commit_binaries {
     echo "TODO: implement me"
 }
--- release_android.sh
+++ release_android.sh
@@ -6,6 +6,7 @@ set -o pipefail
 
 SRC=$(dirname "$(readlink -e "$0")")
 source "${SRC}/build_all.sh"
+source "${SRC}/commit-binaries.sh"
 
 PROJECTS_AIOT=("arm-trusted-firmware" "build" "libbase-prebuilts" "libdram"
                "lk" "optee-os" "optee-ta/kmgk" "optee-ta/optee-otp" "u-boot")
@@ -20,62 +21,6 @@ function check_local_changes {
         fi
         popd
     done
-}
-
-function display_center_msg {
-    local line_length="$1"
-    local msg="$2"
-    local length=${#msg}
-    local pad=$(((line_length - length) / 2))
-
-    printf "%0.s " $(seq 1 ${pad})
-    printf "${msg}"
-
-    # if line_length is an odd number, add 1 to pad
-    if [ $(((pad * 2) + length)) != "${line_length}" ]; then
-        pad=$((pad + 1))
-    fi
-
-    printf "%0.s " $(seq 1 ${pad})
-}
-
-function display_commit_msg_header {
-    local path="$1"
-
-    printf "%0.s#" {1..90}
-    printf "\n##"
-    display_center_msg 86 "COMMIT MESSAGE IN:"
-    printf "##\n##"
-    display_center_msg 86 "${path}"
-    printf "##\n"
-    printf "%0.s#" {1..90}
-    printf "\n\n"
-}
-
-function commit_msg_body {
-    local remote_name=$1 && shift
-    local projects=("$@")
-
-    local remote_url=""
-    local head=""
-    local branch=""
-
-    for project in "${projects[@]}"; do
-        pushd "${ROOT}/${project}"
-        body+="- Project: ${project}:\n"
-
-        remote_url=$(git remote get-url "${remote_name}")
-        body+="URL: ${remote_url}\n"
-
-        branch=$(repo --color=never info . 2>&1 | perl -ne 'print "$1" if /^Manifest revision: (.*)/')
-        body+="Branch: ${branch}\n"
-
-        head=$(git log --oneline --no-decorate -1)
-        body+="HEAD: ${head}\n\n"
-        popd
-    done
-
-    echo "${body}"
 }
 
 function add_commit_msg {
